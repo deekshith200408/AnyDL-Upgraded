e87980d75ece184fdd0dcf74d1d1ba7e2f8d1582
Deeksgowda <75824006+deekshith200408@users.noreply.github.com>
2021-06-01 10:55:50 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,56 +1,62 @@
 class Translation(object):
-    START_TEXT = """This is a Multi Purpose bot created by @princedavitt.
-    
-    This Bot is exclusively made for Cinemaglitz™ Team !
-    Please suscribe first channel.Otherwise you will get currupted file
-    
-    Channel Link : https://t.me/joinchat/AAAAAFP9cMvmpwtK_k2kXg
-    
-© @princedavitt"""
-    RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
-    ABS_TEXT = " Please don't be selfish."
-    UPGRADE_TEXT = "Contact @princedavitt for plans"
-    
-    FORMAT_SELECTION = "Got the file. \n now sent me a image if you want to set as custom thumbnail \n and then click the needed format from the below buttons."
-    SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
-URL | filename | username | password"""
-    NOYES_URL = "@GetPublicLinkBot URL detected. Please do not abuse the service!"
-    DOWNLOAD_START = "📤 Your request is in the queue. Do not send another request. Please be patient..."
-    UPLOAD_START = "Uploading....Join Channel"
-    RCHD_BOT_API_LIMIT = "Size greater than maximum allowed size (50MB). Neverthless, trying to upload."
-    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.96GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "Mission Accomplished!!! Join @Cglitz for any help."
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. And \nUploaded in {} seconds."
-    NOT_AUTH_USER_TEXT = "Please /upgrade your subscription.Contact @princedavitt"
-    NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription."
-    SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
-    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
-    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
-    SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
-    CUSTOM_CAPTION_UL_FILE = " "
-    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
-    NO_VOID_FORMAT_FOUND = "something is wrong with the URL you gave me 🤦‍♀️.\n<b>YouTubeDL</b> said: {}"
-    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
-    CURENT_PLAN_DETAILS = """Current Plan details
---------
-Telegram ID: <code>{}</code>
-Plan name: AnyDl
-Expiry: Ask to @Princedavitt"""
-    HELP_USER = """There are multiple things I can do:
+    START_TEXT = """🤓🙋‍♂️🅷🅸🅸 🅳🅴🅰🆁 ,
 
-👉 All Supported Video / File Formats, with custom file name and custom thumbnail support
-👉 Upload as file from any HTTP link, with custom thumbnail support
-👉 Convert To Streamable Video, any Telegram file
-👉 Convert To Telegram Audio, the media sent as Telegram Documents
-👉 ReName Telegram files, with custom thumbnail support
-👉 Generate Custom Thumbnail by sending two photos in a Media Album
-👉 Trim large videos, and Take Screenshots of Telegram media files.
-👉 Extract compressed Telegram media
-👉 Get a Telegram sticker as a Telegram downloadable media
-👉 Get Direct Download link of any media
-👉 Extract subtitle from any direct links
---------
-Contact @princedavitt for more info."""
+😎ᴛʜɪs ɪs ᴀ ᴍᴜʟᴛɪ ᴘᴜʀᴘᴏsᴇ ʙᴏᴛ 🤖
+
+💪ᴄʀᴇᴀᴛᴇᴅ ʙʏ ➡️ @Deeks_04_8.🕵️
+
+☠️ 𝗧𝗵𝗶𝘀 𝗯𝗼𝘁 𝗖𝗿𝗲𝗮𝘁𝗲 𝗳𝗼𝗿 @UM_Requests 𝗚𝗿𝗼𝘂𝗽 𝗼𝘄𝗻𝗲𝗿  ☠️
+
+       🥳𝑷𝒓𝒊𝒎𝒆 𝒎𝒆𝒎𝒃𝒆𝒓𝒔 𝒂𝒍𝒔𝒐𝒐 𝒖𝒔𝒆 𝒕𝒉𝒊𝒔 𝑩𝒐𝒕 𝒇𝒐𝒓 𝒑𝒂𝒊𝒅 🕵️"""
+    RENAME_403_ERR =
+ "🤓𝗦𝗼𝗿𝗿𝘆. 𝗬𝗼𝘂 𝗮𝗿𝗲 𝗻𝗼𝘁 𝗽𝗲𝗿𝗺𝗶𝘁𝘁𝗲𝗱 𝘁𝗼 𝗿𝗲𝗻𝗮𝗺𝗲 𝘁𝗵𝗶𝘀 𝗳𝗶𝗹𝗲.\n\n👒𝑪𝒐𝒏𝒕𝒂𝒄𝒕 @Deeks_04_8 𝒇𝒐𝒓 𝒎𝒐𝒓𝒆 𝒊𝒏𝒇𝒐.👑"
+    ABS_TEXT = " 𝙋𝙡𝙚𝙖𝙨𝙚 𝙙𝙤𝙣'𝙩 𝙗𝙚 𝙨𝙚𝙡𝙛𝙞𝙨𝙝."
+    UPGRADE_TEXT ="𝐂𝐨𝐧𝐭𝐚𝐜𝐭 @Deeks_04_8 𝐟𝐨𝐫 𝐩𝐥𝐚𝐧𝐬"
+    
+    FORMAT_SELECTION = "🥳𝑮𝒐𝒕 𝒕𝒉𝒆 𝒇𝒊𝒍𝒆. \n 𝒏𝒐𝒘 𝒔𝒆𝒏𝒕 𝒎𝒆 𝒂 𝒊𝒎𝒂𝒈𝒆 𝒊𝒇 𝒚𝒐𝒖 𝒘𝒂𝒏𝒕 𝒕𝒐 𝒔𝒆𝒕 𝒂𝒔 𝒄𝒖𝒔𝒕𝒐𝒎 𝒕𝒉𝒖𝒎𝒃𝒏𝒂𝒊𝒍📸 \n 𝒂𝒏𝒅 𝒕𝒉𝒆𝒏 𝒄𝒍𝒊𝒄𝒌 𝒕𝒉𝒆 𝒏𝒆𝒆𝒅𝒆𝒅 𝒇𝒐𝒓𝒎𝒂𝒕 𝒇𝒓𝒐𝒎 𝒕𝒉𝒆 𝒃𝒆𝒍𝒐𝒘 𝒃𝒖𝒕𝒕𝒐𝒏𝒔.🔎"
+    SET_CUSTOM_USERNAME_PASSWORD = """🔰𝐈𝐟 𝐲𝐨𝐮 𝐰𝐚𝐧𝐭 𝐭𝐨 𝐝𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐩𝐫𝐞𝐦𝐢𝐮𝐦 𝐯𝐢𝐝𝐞𝐨𝐬, 𝐩𝐫𝐨𝐯𝐢𝐝𝐞 𝐢𝐧 𝐭𝐡𝐞 𝐟𝐨𝐥𝐥𝐨𝐰𝐢𝐧𝐠 𝐟𝐨𝐫𝐦𝐚𝐭🔰:
+
+        𝚄𝚁𝙻 | 𝚏𝚒𝚕𝚎𝚗𝚊𝚖𝚎 | 𝚞𝚜𝚎𝚛𝚗𝚊𝚖𝚎 | 𝚙𝚊𝚜𝚜𝚠𝚘𝚛𝚍"""
+    NOYES_URL = "📛@GetPublicLinkBot 𝑼𝑹𝑳 𝒅𝒆𝒕𝒆𝒄𝒕𝒆𝒅. 𝑷𝒍𝒆𝒂𝒔𝒆 𝒅𝒐 𝒏𝒐𝒕 𝒂𝒃𝒖𝒔𝒆 𝒕𝒉𝒆 𝒔𝒆𝒓𝒗𝒊𝒄𝒆!📛"
+    DOWNLOAD_START = "📤 𝗬𝗼𝘂𝗿 𝗿𝗲𝗾𝘂𝗲𝘀𝘁 𝗶𝘀 𝗶𝗻 𝘁𝗵𝗲 𝗾𝘂𝗲𝘂𝗲. \n📛𝗗𝗼 𝗻𝗼𝘁 𝘀𝗲𝗻𝗱 𝗮𝗻𝗼𝘁𝗵𝗲𝗿 𝗿𝗲𝗾𝘂𝗲𝘀𝘁. 𝗣𝗹𝗲𝗮𝘀𝗲 𝗯𝗲 𝗽𝗮𝘁𝗶𝗲𝗻𝘁...📛"
+    UPLOAD_START = "🥳𝐔𝐩𝐥𝐨𝐚𝐝𝐢𝐧𝐠 𝐒𝐭𝐚𝐫𝐭𝐞𝐝 𝐩𝐥𝐳 𝐬𝐭𝐚𝐲 𝐭𝐮𝐧𝐞𝐝...😍"
+    RCHD_BOT_API_LIMIT = "😟𝕾𝖎𝖟𝖊 𝖌𝖗𝖊𝖆𝖙𝖊𝖗 𝖙𝖍𝖆𝖓 𝖒𝖆𝖝𝖎𝖒𝖚𝖒 𝖆𝖑𝖑𝖔𝖜𝖊𝖉 𝖘𝖎𝖟𝖊 (50MB). 𝕹𝖊𝖛𝖊𝖗𝖙𝖍𝖑𝖊𝖘𝖘, 𝖙𝖗𝖞𝖎𝖓𝖌 𝖙𝖔 𝖚𝖕𝖑𝖔𝖆𝖉.😤"
+    RCHD_TG_API_LIMIT = "🔰𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝𝐞𝐝 𝐢𝐧 {} 𝐬𝐞𝐜𝐨𝐧𝐝𝐬.\n📛𝐃𝐞𝐭𝐞𝐜𝐭𝐞𝐝 𝐅𝐢𝐥𝐞 𝐒𝐢𝐳𝐞: {}\n🤓𝐒𝐨𝐫𝐫𝐲. 𝐁𝐮𝐭, 𝐈 𝐜𝐚𝐧𝐧𝐨𝐭 𝐮𝐩𝐥𝐨𝐚𝐝 𝐟𝐢𝐥𝐞𝐬 𝐠𝐫𝐞𝐚𝐭𝐞𝐫 𝐭𝐡𝐚𝐧 1.96𝐆𝐁 𝐝𝐮𝐞 𝐭𝐨 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐀𝐏𝐈 𝐥𝐢𝐦𝐢𝐭𝐚𝐭𝐢𝐨𝐧𝐬.😔"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "🥳🤓𝙈𝙞𝙨𝙨𝙞𝙤𝙣 𝘼𝙘𝙘𝙤𝙢𝙥𝙡𝙞𝙨𝙝𝙚𝙙!!! 𝙅𝙤𝙞𝙣 @UM_Requests 𝙛𝙤𝙧 𝙖𝙣𝙮 𝙝𝙚𝙡𝙥😎🔰."
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "📛𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝𝐞𝐝 𝐢𝐧 {} 𝐬𝐞𝐜𝐨𝐧𝐝𝐬.\n\n🔰𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐝 𝐢𝐧 {} 𝐬𝐞𝐜𝐨𝐧𝐝𝐬."
+    NOT_AUTH_USER_TEXT = "🙏𝗣𝗹𝗲𝗮𝘀𝗲 /𝘂𝗽𝗴𝗿𝗮𝗱𝗲 𝘆𝗼𝘂𝗿 𝘀𝘂𝗯𝘀𝗰𝗿𝗶𝗽𝘁𝗶𝗼𝗻.𝗖𝗼𝗻𝘁𝗮𝗰𝘁 @Deeks_04_8😎"
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "🕵️𝐃𝐞𝐭𝐞𝐜𝐭𝐞𝐝 𝐅𝐢𝐥𝐞 𝐒𝐢𝐳𝐞: {}.\n😜𝐅𝐫𝐞𝐞 𝐔𝐬𝐞𝐫𝐬 𝐜𝐚𝐧 𝐨𝐧𝐥𝐲 𝐮𝐩𝐥𝐨𝐚𝐝: {}\n🤑𝐏𝐥𝐞𝐚𝐬𝐞 /𝐮𝐩𝐠𝐫𝐚𝐝𝐞 𝐲𝐨𝐮𝐫 𝐬𝐮𝐛𝐬𝐜𝐫𝐢𝐩𝐭𝐢𝐨𝐧."
+    SAVED_CUSTOM_THUMB_NAIL ="🎬𝙲𝚞𝚜𝚝𝚘𝚖 𝚟𝚒𝚍𝚎𝚘 / 𝚏𝚒𝚕𝚎 𝚝𝚑𝚞𝚖𝚋𝚗𝚊𝚒𝚕 𝚜𝚊𝚟𝚎𝚍🕯️. \n🔰𝚃𝚑𝚒𝚜 𝚒𝚖𝚊𝚐𝚎 𝚠𝚒𝚕𝚕 𝚋𝚎 𝚞𝚜𝚎𝚍 𝚒𝚗 𝚝𝚑𝚎 𝚟𝚒𝚍𝚎𝚘 / 𝚏𝚒𝚕𝚎."
+    DEL_ETED_CUSTOM_THUMB_NAIL = "〽️ 𝐂𝐮𝐬𝐭𝐨𝐦 𝐭𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥 𝐜𝐥𝐞𝐚𝐫𝐞𝐝 𝐬𝐮𝐜𝐜𝐞𝐬𝐟𝐮𝐥𝐥𝐲🚮."
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "〽️ 𝐌𝐞𝐝𝐢𝐚 𝐜𝐥𝐞𝐚𝐫𝐞𝐝 𝐬𝐮𝐜𝐜𝐞𝐬𝐟𝐮𝐥𝐥𝐲🚮."
+    SAVED_RECVD_DOC_FILE = "🎦𝗗𝗼𝗰𝘂𝗺𝗲𝗻𝘁 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱𝗲𝗱 𝗦𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆.📶"
+    CUSTOM_CAPTION_UL_FILE = "⚜️𝙹𝙾𝙸𝙽 𝙾𝚄𝚁 𝙰𝙻𝙻 𝙲𝙷𝙰𝙽𝙽𝙴𝙻𝚂 ♻️@UNI_MOVIE_BOX♻️ "
+    NO_CUSTOM_THUMB_NAIL_FOUND = "🙅‍♂️𝙉𝙤 𝘾𝙪𝙨𝙩𝙤𝙢 𝙏𝙝𝙪𝙢𝙗𝙉𝙖𝙞𝙡 𝙛𝙤𝙪𝙣𝙙.❗️"
+    NO_VOID_FORMAT_FOUND = "🤔𝐬𝐨𝐦𝐞𝐭𝐡𝐢𝐧𝐠 𝐢𝐬 𝐰𝐫𝐨𝐧𝐠 𝐰𝐢𝐭𝐡 𝐭𝐡𝐞 𝐔𝐑𝐋 𝐲𝐨𝐮 𝐠𝐚𝐯𝐞 𝐦𝐞 🤦‍♀️.\n<b>𝗬𝗼𝘂𝗧𝘂𝗯𝗲𝗗𝗟</b> 𝐬𝐚𝐢𝐝: {}"
+    USER_ADDED_TO_DB = "😎𝚄𝚜𝚎𝚛 <a href='tg://user?id={}'>{}</a> 𝚊𝚍𝚍𝚎𝚍 𝚝𝚘 {} till {}.🥳"
+    CURENT_PLAN_DETAILS = """💡𝗖𝘂𝗿𝗿𝗲𝗻𝘁 𝗣𝗹𝗮𝗻 𝗱𝗲𝘁𝗮𝗶𝗹𝘀
+
+💎𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐈𝐃: <code>{}</code>
+💎𝐏𝐥𝐚𝐧 𝐧𝐚𝐦𝐞: 𝐀𝐧𝐲𝐃𝐥
+💎𝐄𝐱𝐩𝐢𝐫𝐲: 𝐀𝐬𝐤 𝐭𝐨 @Deeks_04_8"""
+    HELP_USER = """💡 𝗛𝗘𝗟𝗣,
+
+🎓𝐓𝐡𝐞𝐫𝐞 𝐚𝐫𝐞 𝐦𝐮𝐥𝐭𝐢𝐩𝐥𝐞 𝐭𝐡𝐢𝐧𝐠𝐬 𝐈 𝐜𝐚𝐧 𝐝𝐨 :-🎩
+
+🎲 <b>ᴀʟʟ sᴜᴘᴘᴏʀᴛᴇᴅ ᴠɪᴅᴇᴏ / ғɪʟᴇ ғᴏʀᴍᴀᴛs, ᴡɪᴛʜ ᴄᴜsᴛᴏᴍ ғɪʟᴇ ɴᴀᴍᴇ ᴀɴᴅ ᴄᴜsᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ sᴜᴘᴘᴏʀᴛ,
+🎲 ᴜᴘʟᴏᴀᴅ ᴀs ғɪʟᴇ ғʀᴏᴍ ᴀɴʏ ʜᴛᴛᴘ ʟɪɴᴋ, ᴡɪᴛʜ ᴄᴜsᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ sᴜᴘᴘᴏʀᴛ,
+🎲 ᴄᴏɴᴠᴇʀᴛ ᴛᴏ sᴛʀᴇᴀᴍᴀʙʟᴇ ᴠɪᴅᴇᴏ, ᴀɴʏ ᴛᴇʟᴇɢʀᴀᴍ ғɪʟᴇ,
+🎲 ᴄᴏɴᴠᴇʀᴛ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴍ ᴀᴜᴅɪᴏ, ᴛʜᴇ ᴍᴇᴅɪᴀ sᴇɴᴛ ᴀs ᴛᴇʟᴇɢʀᴀᴍ ᴅᴏᴄᴜᴍᴇɴᴛs,
+🎲 ʀᴇɴᴀᴍᴇ ᴛᴇʟᴇɢʀᴀᴍ ғɪʟᴇs, ᴡɪᴛʜ ᴄᴜsᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ sᴜᴘᴘᴏʀᴛ,
+🎲 ɢᴇɴᴇʀᴀᴛᴇ ᴄᴜsᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ ʙʏ sᴇɴᴅɪɴɢ ᴛᴡᴏ ᴘʜᴏᴛᴏs ɪɴ ᴀ ᴍᴇᴅɪᴀ ᴀʟʙᴜᴍ,
+🎲 ᴛʀɪᴍ ʟᴀʀɢᴇ ᴠɪᴅᴇᴏs, ᴀɴᴅ ᴛᴀᴋᴇ sᴄʀᴇᴇɴsʜᴏᴛs ᴏғ ᴛᴇʟᴇɢʀᴀᴍ ᴍᴇᴅɪᴀ ғɪʟᴇs.
+🎲 ᴇxᴛʀᴀᴄᴛ ᴄᴏᴍᴘʀᴇssᴇᴅ ᴛᴇʟᴇɢʀᴀᴍ ᴍᴇᴅɪᴀ,
+🎲 ɢᴇᴛ ᴀ ᴛᴇʟᴇɢʀᴀᴍ sᴛɪᴄᴋᴇʀ ᴀs ᴀ ᴛᴇʟᴇɢʀᴀᴍ ᴅᴏᴡɴʟᴏᴀᴅᴀʙʟᴇ ᴍᴇᴅɪᴀ,
+🎲 ɢᴇᴛ ᴅɪʀᴇᴄᴛ ᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ ᴏғ ᴀɴʏ ᴍᴇᴅɪᴀ, 
+🎲 ᴇxᴛʀᴀᴄᴛ sᴜʙᴛɪᴛʟᴇ ғʀᴏᴍ ᴀɴʏ ᴅɪʀᴇᴄᴛ ʟɪɴᴋs,</b>
+
+👒𝑪𝒐𝒏𝒕𝒂𝒄𝒕 @Deeks_04_8 𝒇𝒐𝒓 𝒎𝒐𝒓𝒆 𝒊𝒏𝒇𝒐.👑
+."""
    
     REPLY_TO_DOC_GET_LINK = "Reply to a Telegram media to get High Speed Direct Download Link"
     REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
